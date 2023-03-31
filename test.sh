c310d7988c6d472daccd61282f11a01ad39602f6
Nicknjf1 <Nicknjf1@gmail.com>
2023-03-31 14:53:19 -0500

    New Changes to test.sh

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#############################
+
+# test.sh
+
+#
+
+# This script
+
+#
+
+# Usage: sudo ./test.sh
+
+################
+
+# Initial Line
+
+echo "Initial Comment"
+
+echo "New changes"
+
